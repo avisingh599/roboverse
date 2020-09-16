e4a12316bb85cb9e0162ae176a48383f9b0549ab
Albert Yu <albertyu@berkeley.edu>
2020-09-16 16:16:16 -0700

    Add reward, get_info methods for drawer opening env. Add drawer opened percentage to info.

--- roboverse/bullet/drawer_utils.py
+++ roboverse/bullet/drawer_utils.py
@@ -1,31 +1,43 @@
 import pybullet as p
+import roboverse.bullet as bullet
 import roboverse.bullet.control as control
 import numpy as np
 
 
 def open_drawer(drawer):
-    slide_drawer(drawer, -1)
+    return slide_drawer(drawer, -1)
 
 
 def close_drawer(drawer):
-    slide_drawer(drawer, 1)
+    return slide_drawer(drawer, 1)
+
+
+def get_drawer_base_joint(drawer):
+    joint_names = [control.get_joint_info(drawer, j, 'jointName')
+                   for j in range(p.getNumJoints(drawer))]
+    drawer_frame_joint_idx = joint_names.index('base_frame_joint')
+    return drawer_frame_joint_idx
+
+
+def get_drawer_bottom_pos(drawer):
+    drawer_bottom_pos, _ = bullet.get_link_state(
+        drawer, get_drawer_base_joint(drawer))
+    return np.array(drawer_bottom_pos)
 
 
 def slide_drawer(drawer, direction):
     assert direction in [-1, 1]
     # -1 = open; 1 = close
-    joint_names = [control.get_joint_info(drawer, j, 'jointName')
-                   for j in range(p.getNumJoints(drawer))]
-    drawer_frame_joint_idx = joint_names.index('base_frame_joint')
+    drawer_frame_joint_idx = get_drawer_base_joint(drawer)
 
-    num_ts = 15 if direction == -1 else 25
+    num_ts = 20 if direction == -1 else 30
 
     command = np.clip(10 * direction,
                       -10 * np.abs(direction), np.abs(direction))
     # enable fast opening; slow closing
 
     # Wait a little before closing
-    wait_ts = 0 if direction == -1 else 20
+    wait_ts = 30  # 0 if direction == -1 else 30
     control.step_simulation(wait_ts, t_sleep=0.01)
 
     p.setJointMotorControl2(
@@ -36,6 +48,8 @@ def slide_drawer(drawer, direction):
         force=10
     )
 
+    drawer_pos = get_drawer_bottom_pos(drawer)
+
     control.step_simulation(num_ts, t_sleep=0.01)
     p.setJointMotorControl2(
         drawer,
@@ -44,3 +58,5 @@ def slide_drawer(drawer, direction):
         targetVelocity=0,
         force=10
     )
+
+    return drawer_pos
--- roboverse/envs/widow250.py
+++ roboverse/envs/widow250.py
@@ -366,16 +366,79 @@ class Widow250PickPlaceEnv(Widow250Env):
 
 class Widow250DrawerEnv(Widow250Env):
 
-    def __init__(self, **kwargs):
-        super(Widow250DrawerEnv, self).__init__(load_tray=False, **kwargs)
+    def __init__(self,
+                 object_names=("ball",),
+                 object_scales=(0.75,),
+                 object_orientations=((0, 0, 1, 0),),
+                 reward_type="opening",
+                 **kwargs):
+        self.drawer_pos = (0.6, 0.2, -.35)
+        obj_pos_high = tuple(
+            np.array(self.drawer_pos[:2] + (-.2,)) + np.array((0.1, 0, 0)))
+        obj_pos_low = tuple(
+            np.array(self.drawer_pos[:2] + (-.2,)) - np.array((-0.1, 0, 0)))
+        super(Widow250DrawerEnv, self).__init__(
+            object_names=object_names,
+            object_scales=object_scales,
+            object_orientations=object_orientations,
+            object_position_high=obj_pos_high,
+            object_position_low=obj_pos_low,
+            target_object=object_names[0],
+            load_tray=False,
+            reward_type=reward_type,
+            **kwargs
+        )
 
     def _load_meshes(self):
-        super(Widow250DrawerEnv, self)._load_meshes()
+        self.table_id = objects.table()
+        self.robot_id = objects.widow250()
+
+        if self.load_tray:
+            self.tray_id = objects.tray()
+
+        self.objects = {}
+        object_positions = object_utils.generate_object_positions(
+            self.object_position_low, self.object_position_high,
+            self.num_objects,
+        )
+        self.original_object_positions = object_positions
+
         self.objects["drawer"] = object_utils.load_object(
-            "drawer", (0.7, 0.3, -.35), (0, 0, 0.707107, 0.707107), scale=0.1)
+            "drawer", self.drawer_pos, (0, 0, 0.707107, 0.707107), scale=0.1)
         # Open and close testing.
-        object_utils.open_drawer(self.objects['drawer'])
-        object_utils.close_drawer(self.objects['drawer'])
+        self.drawer_min_y_pos = object_utils.open_drawer(
+            self.objects['drawer'])[0]
+
+        for object_name, object_position in zip(self.object_names,
+                                                object_positions):
+            self.objects[object_name] = object_utils.load_object(
+                object_name,
+                object_position,
+                object_quat=self.object_orientations[object_name],
+                scale=self.object_scales[object_name])
+            bullet.step_simulation(self.num_sim_steps_reset)
+
+        self.drawer_max_y_pos = object_utils.close_drawer(
+            self.objects['drawer'])[0]
+
+    def get_info(self):
+        info = super(Widow250DrawerEnv, self).get_info()
+        drawer_y_pos = object_utils.get_drawer_bottom_pos(
+            self.objects["drawer"])[0]
+        info['drawer_y_pos'] = drawer_y_pos
+        info['drawer_opened_percentage'] = (
+            np.abs(drawer_y_pos - self.drawer_min_y_pos) /
+            (self.drawer_max_y_pos - self.drawer_min_y_pos)
+        )
+        return info
+
+    def get_reward(self, info):
+        if not info:
+            info = self.get_info()
+        if self.reward_type == "opening":
+            return float(info["drawer_opened_percentage"] > 0.7)
+        else:
+            return super(Widow250DrawerEnv, self).get_reward(info)
 
 
 if __name__ == "__main__":
@@ -387,7 +450,9 @@ if __name__ == "__main__":
 
     for i in range(20):
         print(i)
-        env.step(np.asarray([-0.05, 0., 0., 0., 0., 0.5, 0.]))
+        obs, rew, done, info = env.step(
+            np.asarray([-0.05, 0., 0., 0., 0., 0.5, 0.]))
+        print("reward", rew, "info", info)
         time.sleep(0.1)
 
     env.reset()
