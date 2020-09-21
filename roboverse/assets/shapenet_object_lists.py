4879629965cf475ee9934f7a2a3bcb5a8a46125d
Avi Singh <avisingh599@gmail.com>
2020-09-20 17:06:36 -0700

    Add test env for pickplace, add configs for torus and cube

--- roboverse/assets/shapenet_object_lists.py
+++ roboverse/assets/shapenet_object_lists.py
@@ -1,12 +1,22 @@
 TRAIN_CONTAINERS = [
     'plate',
     'cube_concave',
-    'table_top'
+    'table_top',
+    'cube',
+    'open_box',
+    'tray',
+    'torus',
+    'husky',
+]
+
+TEST_CONTAINERS = [
+    'bowl_small',
+    'pan_tefal',
+    'marble_cube',
 ]
 
 CONTAINER_CONFIGS = {
     'plate': {
-        'container_name': 'plate',
         'container_position_low': (.50, 0.22, -.30),
         'container_position_high': (.70, 0.26, -.30),
         'container_orientation': (0, 0, 0.707107, 0.707107),
@@ -28,7 +38,6 @@ CONTAINER_CONFIGS = {
         'min_distance_from_object': 0.10,
     },
     'table_top': {
-        'container_name': 'table_top',
         'container_position_low': (.50, 0.22, -.30),
         'container_position_high': (.70, 0.26, -.30),
         'container_orientation': (0, 0, 0.707107, 0.707107),
@@ -39,7 +48,6 @@ CONTAINER_CONFIGS = {
         'min_distance_from_object': 0.11,
     },
     'bowl_small': {
-        'container_name': 'bowl_small',
         'container_position_low': (.5, 0.26, -.30),
         'container_position_high': (.7, 0.26, -.30),
         'container_position_z': -0.35,
@@ -50,7 +58,6 @@ CONTAINER_CONFIGS = {
         'min_distance_from_object': 0.09,
     },
     'tray': {
-        'container_name': 'tray',
         'container_position_low': (.5, 0.25, -.30),
         'container_position_high': (.7, 0.25, -.30),
         'container_orientation': (0, 0, 0.707107, 0.707107),
@@ -61,7 +68,6 @@ CONTAINER_CONFIGS = {
         'min_distance_from_object': 0.1,
     },
     'open_box': {
-        'container_name': 'open_box',
         'container_position_low': (.5, 0.23, -.30),
         'container_position_high': (.7, 0.23, -.30),
         'container_orientation': (0, 0, 0.707107, 0.707107),
@@ -71,8 +77,7 @@ CONTAINER_CONFIGS = {
         'place_success_radius_threshold': 0.04,
         'min_distance_from_object': 0.1,
     },
-    'plate_tefal': {
-        'container_name': 'pan_tefal',
+    'pan_tefal': {
         'container_position_low': (.50, 0.22, -.30),
         'container_position_high': (.70, 0.24, -.30),
         'container_orientation': (0, 0, 0.707107, 0.707107),
@@ -83,7 +88,6 @@ CONTAINER_CONFIGS = {
         'min_distance_from_object': 0.09,
     },
     'husky': {
-        'container_name': 'husky',
         'container_position_low': (.50, 0.22, -.30),
         'container_position_high': (.70, 0.26, -.30),
         'container_orientation': (0, 0, 0.707107, 0.707107),
@@ -94,7 +98,6 @@ CONTAINER_CONFIGS = {
         'min_distance_from_object': 0.10,
     },
     'marble_cube': {
-        'container_name': 'marble_cube',
         'container_position_low': (.50, 0.22, -.30),
         'container_position_high': (.70, 0.26, -.30),
         'container_orientation': (0, 0, 0.707107, 0.707107),
@@ -103,6 +106,26 @@ CONTAINER_CONFIGS = {
         'place_success_height_threshold': -0.23,
         'place_success_radius_threshold': 0.04,
         'min_distance_from_object': 0.10,
+    },
+    'torus': {
+        'container_position_low': (.50, 0.22, -.30),
+        'container_position_high': (.70, 0.26, -.30),
+        'container_orientation': (1, 1, 1, 1),
+        'container_scale': 0.15,
+        'container_position_z': -0.35,
+        'place_success_height_threshold': -0.32,
+        'place_success_radius_threshold': 0.04,
+        'min_distance_from_object': 0.09,
+    },
+    'cube': {
+        'container_position_low': (.5, 0.22, -.30),
+        'container_position_high': (.7, 0.24, -.30),
+        'container_orientation': (0, 0, 0.707107, 0.707107),
+        'container_scale': 0.05,
+        'container_position_z': -0.35,
+        'place_success_radius_threshold': 0.03,
+        'place_success_height_threshold': -0.23,
+        'min_distance_from_object': 0.09,
     }
 
 }
