65eaa1f08bfeb0e68151fc8b8224319741fab8aa
Jonathan Yang <jy2370@berkeley.edu>
2020-09-21 13:59:22 -0700

    Modify keyboard control script

--- scripts/keyboard_control.py
+++ scripts/keyboard_control.py
@@ -1,13 +1,12 @@
 import numpy as np
 import time
+import argparse
 
 import roboverse
 from roboverse.envs.widow250 import Widow250Env
 import roboverse.bullet as bullet
 
-env = roboverse.make('Widow250MultiTaskGrasp-v0', gui=True)
-
-keys_pressed = {bullet.p.B3G_LEFT_ARROW: np.array([0.1, 0, 0, 0, 0, 0, 0]),
+action_commands = {bullet.p.B3G_LEFT_ARROW: np.array([0.1, 0, 0, 0, 0, 0, 0]),
 bullet.p.B3G_RIGHT_ARROW: np.array([-0.1, 0, 0, 0, 0, 0, 0]),
 bullet.p.B3G_UP_ARROW: np.array([0, -0.1, 0, 0, 0, 0, 0]),
 bullet.p.B3G_DOWN_ARROW: np.array([0, 0.1, 0, 0, 0, 0, 0]),
@@ -16,14 +15,31 @@ ord('k'): np.array([0, 0, -0.2, 0, 0, 0, 0]),
 ord('h'): np.array([0, 0, 0, 0, 0, 0, -0.7]),
 ord('l'): np.array([0, 0, 0, 0, 0, 0, 0.7])}
 
-while True:
-    pressed = False
-    action = np.array([0, 0, 0, 0, 0, 0, 0], dtype='float32')
-    keys = bullet.p.getKeyboardEvents()
-    for qKey in keys_pressed.keys():
-        if qKey in keys:
-            action += keys_pressed[qKey]
-            pressed = True
-    if pressed:
-        env.step(action)
-    time.sleep(0.1)
+env_commands = {
+ord('r'): lambda env: env.reset()
+}
+
+def keyboard_control(args):
+    env = roboverse.make(args.env_name, gui=True)
+
+    while True:
+        take_action = False
+        action = np.array([0, 0, 0, 0, 0, 0, 0], dtype='float32')
+        keys = bullet.p.getKeyboardEvents()
+        for qKey in keys:
+            if qKey in action_commands.keys():
+                action += action_commands[qKey]
+                take_action = True
+            elif qKey in env_commands.keys():
+                env_commands[qKey](env)
+                take_action = False
+
+        if take_action:
+            env.step(action)
+        time.sleep(0.1)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--env-name", type=str, default='Widow250MultiTaskGrasp-v0')
+    args = parser.parse_args()
+    keyboard_control(args)
