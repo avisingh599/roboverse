0aef8844785cf0f61f31010a9b156b42fd65fcb8
Jonathan Yang <jy2370@berkeley.edu>
2020-09-26 11:23:16 -0700

    Fix bug with importing deleted DrawerGrasp script

--- roboverse/policies/__init__.py
+++ roboverse/policies/__init__.py
@@ -2,12 +2,10 @@ from .pick_place import PickPlace
 from .drawer_open import DrawerOpen
 from .grasp import Grasp
 from .button_press import ButtonPress
-from.drawer_grasp import DrawerGrasp
 
 policies = dict(
     grasp=Grasp,
     pickplace=PickPlace,
     drawer_open=DrawerOpen,
     button_press=ButtonPress,
-    drawer_grasp=DrawerGrasp
 )
