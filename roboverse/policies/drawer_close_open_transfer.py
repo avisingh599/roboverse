142150a1bd32b4d1201644a79d90755c1c347448
Avi Singh <avisingh599@gmail.com>
2020-10-13 00:05:45 -0700

    Revamp close-open scripted policy

--- roboverse/policies/drawer_close_open_transfer.py
+++ roboverse/policies/drawer_close_open_transfer.py
@@ -1,16 +1,20 @@
 import numpy as np
 import roboverse.bullet as bullet
+from .drawer_open_transfer import DrawerOpenTransfer
 
 
 class DrawerCloseOpenTransfer:
 
-    def __init__(self, env):
+    def __init__(self, env, suboptimal=False):
         self.env = env
         self.xyz_action_scale = 7.0
         self.gripper_dist_thresh = 0.06
         self.gripper_xy_dist_thresh = 0.04
         self.ending_z = -0.25
         self.top_drawer_offset = np.array([0, 0, 0.02])
+        self.suboptimal = suboptimal
+        self.bottom_drawer_policy = DrawerOpenTransfer(env, suboptimal=self.suboptimal)
+
         self.reset()
 
     def reset(self):
@@ -18,6 +22,9 @@ class DrawerCloseOpenTransfer:
         offset_coeff = (-1) ** (1 - self.env.left_opening)
         self.handle_offset = np.array([offset_coeff * 0.01, 0.0, -0.01])
         self.reached_pushing_region = False
+        self.neutral_taken = False
+
+        self.bottom_drawer_policy.reset()
 
     def get_action(self):
         ee_pos, _ = bullet.get_link_state(
@@ -49,85 +56,21 @@ class DrawerCloseOpenTransfer:
             action_xyz[1] *= 0.6
             action_angles = [0., 0., 0.]
             action_gripper = [0.0]
-        elif (gripper_handle_xy_dist > self.gripper_xy_dist_thresh
-                and not self.env.is_drawer_open()):
-            # print('xy - approaching handle')
-            action_xyz = (handle_pos - ee_pos) * 7.0
-            action_xyz = list(action_xyz[:2]) + [0.]  # don't droop down.
-            action_angles = [0., 0., 0.]
-            action_gripper = [0.0]
-        elif (gripper_handle_dist > self.gripper_dist_thresh
-                and not self.env.is_drawer_open()):
-            # moving down toward handle
-            action_xyz = (handle_pos - ee_pos) * 7.0
-            action_angles = [0., 0., 0.]
-            action_gripper = [0.0]
-        elif not self.env.is_drawer_open():
-            # print("opening drawer")
-            x_command = (-1) ** (1 - self.env.left_opening)
-            action_xyz = np.array([x_command, 0, 0])
-            # action = np.asarray([0., 0., 0.7])
-            action_angles = [0., 0., 0.]
-            action_gripper = [0.0]
-        elif ee_pos[2] < self.ending_z:
-            action_xyz = [0., 0., 0.5]
-            action_angles = [0., 0., 0.]
-            action_gripper = [0.0]
         else:
-            action_xyz = [0., 0., 0.]
-            action_angles = [0., 0., 0.]
-            action_gripper = [0.0]
-            neutral_action = [0.7]
-            done = True
+            action, info = self.bottom_drawer_policy.get_action()
+            action_xyz = action[:3]
+            action_angles = action[3:6]
+            action_gripper = [action[6]]
+            neutral_action = [action[7]]
+            done = info['done']
 
         agent_info = dict(done=done)
         action = np.concatenate((action_xyz, action_angles, action_gripper, neutral_action))
         return action, agent_info
 
 
-class DrawerCloseOpenTransferSuboptimal:
-    def __init__(self, env):
-        self.env = env
-        self.gripper_xy_dist_thresh = 0.04
-        self.height_thresh = -0.2
-        self.reset()
-
-    def reset(self):
-        offset_coeff = (-1) ** (1 - self.env.left_opening)
-        self.handle_offset = np.array([offset_coeff * 0.01, 0.0, -0.01])
-        self.reached_pushing_region = False
-
-    def get_action(self):
-        ee_pos, _ = bullet.get_link_state(
-            self.env.robot_id, self.env.end_effector_index)
-        handle_pos = self.env.get_drawer_handle_pos() + self.handle_offset
-        gripper_handle_xy_dist = np.linalg.norm(handle_pos[:2] - ee_pos[:2])
-        done = False
-        neutral_action = [0.0]
-        if not gripper_handle_xy_dist > self.gripper_xy_dist_thresh:
-            print("move forward")
-            action_xyz = [0., 0.2, -0.3]
-            action_angles = [0., 0., 0.]
-            action_gripper = [0.0]
-        elif not self.env.is_top_drawer_closed() and ee_pos[2] < self.height_thresh:
-            print("open top drawer")
-            self.reached_pushing_region = True
-            action_xyz = [0.5, 0, 0.0]
-            action_angles = [0., 0., 0.]
-            action_gripper = [0.0]
-        elif (gripper_handle_xy_dist > self.gripper_xy_dist_thresh
-                and not self.env.is_drawer_open()):
-            print('xy - approaching handle')
-            action_xyz = (handle_pos - ee_pos) * 7.0
-            action_xyz = list(action_xyz[:2]) + [0.]  # don't droop down.
-            action_angles = [0., 0., 0.]
-            action_gripper = [0.0]
-        else:
-            action_xyz = [0.,0., 0.]  # don't droop down.
-            action_angles = [0., 0., 0.]
-            action_gripper = [0.0]
-
-        agent_info = dict(done=done)
-        action = np.concatenate(
-            (action_xyz, action_angles, action_gripper, neutral_action))
-        return action, agent_info
+class DrawerCloseOpenTransferSuboptimal(DrawerCloseOpenTransfer):
+    def __init__(self, env, **kwargs):
+        super(DrawerCloseOpenTransferSuboptimal, self).__init__(
+            env, suboptimal=True, **kwargs
+        )
